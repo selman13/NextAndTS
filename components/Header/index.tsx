87ce5402d4ae1a9d807a83a21d62bce1bf832c58
selman13 <selmaneliyev99@gmal.com>
2024-10-09 18:34:26 +0400

    logo added

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container'>
+      <div className="logo">
+        <img src="/images/logo.svg" alt="" />
+      </div>
+    </div>
   )
 }
 
